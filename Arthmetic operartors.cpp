86025b4dd578099ff123201300ca57dabbfbfb38
Ishaan <146927801+Ishaan-coder-1@users.noreply.github.com>
2023-10-05 15:13:50 +0530

    Create Arithmetic operartors.cpp

---
+++ Arthmetic operartors.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main () {
+                
+    int student = 20;
+
+    //student+= 1; addition
+    //student -=1; subtraction
+    student *= 2; multiplication
+    //students/= 2; division
+
+    std::cout << student;
+
+
+    return 0;
+}
+
+//int only gives out a whole number but the 'double' function gives decimals
+//to find remainder type "int remaineder = students % 2"
+//% this is calle the modulus operartor
