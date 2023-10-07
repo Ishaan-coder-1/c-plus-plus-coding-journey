c17ce7ec9831260c44594666f9e94e24e7b5b5ad
Ishaan <146927801+Ishaan-coder-1@users.noreply.github.com>
2023-10-07 18:26:48 +0530

    Create switches.cpp

---
+++ switches.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <cmath>
+
+int main () {
+
+    int marks;
+    std::cout << "How many marks id you get (0 -30)? ";
+    std::cin >> marks;
+
+    switch (marks){
+        case 0:
+            std::cout << "You FAILED";
+            break;
+        case 1:
+            std::cout << "You FAILED";
+            break;
+        case 2:
+            std::cout << "You FAILED";
+            break;
+        case 3:
+            std::cout << "You FAILED";
+            break;
+        case 4:
+            std::cout << "You FAILED";
+            break;
+        case 5:
+            std::cout << "You FAILED";
+            break;
+        case 6:
+            std::cout << "You FAILED";
+            break;
+        case 7:
+            std::cout << "You FAILED";
+            break;
+        case 8:
+            std::cout << "You FAILED";
+            break;
+        case 9:
+            std::cout << "You FAILED";
+            break;
+        case 10:
+            std::cout << "You FAILED";
+            break;
+        case 11:
+            std::cout << "You FAILED";
+            break;
+        case 12:
+            std::cout << "You FAILED";
+            break;
+        case 13:
+            std::cout << "You FAILED";
+            break;
+        case 14:
+            std::cout << "You FAILED";
+            break;
+        case 15:
+            std::cout << "You FAILED";
+            break;
+        case 16:
+            std::cout << "You Did bad";
+            break;
+        case 17:
+            std::cout << "You Did bad";
+            break;
+        case 18:
+            std::cout << "You Did bad";
+            break;
+        case 19:
+            std::cout << "You Did bad";
+            break;
+        case 20:
+            std::cout << "You okay";
+            break;
+        case 21:
+            std::cout << "You okay";
+            break;
+        case 22:
+            std::cout << "You okay";
+            break;
+        case 23:
+            std::cout << "You good";
+            break;
+        case 24:
+            std::cout << "You good";
+            break;
+        case 25:
+            std::cout << "You good";
+            break;
+        case 26:
+            std::cout << "You great";
+            break;
+        case 27:
+            std::cout << "You great";
+            break;
+        case 28:
+            std::cout << "You VERY GOOD";
+            break;
+        case 29:
+            std::cout << "You VERY GOOD";
+            break;
+        case 30:
+            std::cout << "You VERY GOOD";
+            break;
+        default:
+        std::cout << "Please enter a number (0-30)";
+
+        
+
+    }
+
+
+    return 0;
+}
+
+// it is used instead of else, if and esle if
