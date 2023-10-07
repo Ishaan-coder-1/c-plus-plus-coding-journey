2b60041e87dbf31316253ab0ec75c521b99cef40
Ishaan <146927801+Ishaan-coder-1@users.noreply.github.com>
2023-10-07 20:42:01 +0530

    Create Ternary_operator.cpp

---
+++ Ternary_operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main () {
+
+    int grade = 75;
+    
+    grade >= 60 ? std::cout << "you pass!" : std::cout << "you fail! ";
+
+    return 0;
+}
+
+// it is used instead of else if statements
+// int can be used in the form of critera ? the thing to if its true : the you wnt to do if its false
