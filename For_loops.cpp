5203e3215b1f7cd9fa1b8790cee1b5ee6fffd5fa
Ishaan <146927801+Ishaan-coder-1@users.noreply.github.com>
2023-10-09 20:18:54 +0530

    Create For_loops.cpp

---
+++ For_loops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main () {
+
+    for(int index = 0; index <= 10; index+=1){
+        std::cout << index << '\n';
+    }
+
+    std::cout << "Happy new year! \n";
+
+    return 0;
+}
+
+// it takes in consideration of three factors and does it in a loop
